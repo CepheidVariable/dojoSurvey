8a79185c9c2ce568935016b3ac06420bf9f88ae8
CepheidVariable <robertwoodard.usa@gmail.com>
2021-01-11 01:04:51 -0600

    added session + redirect route

--- server.py
+++ server.py
@@ -1,37 +1,45 @@
 from flask import Flask, render_template, request, redirect, session
 import os
-app = Flask(__name__)
 
+def gen_hash(byte_size=16):
+    '''Generates a 16-bit hash by default.'''
+    hash_result = os.urandom(byte_size)
+    return redirect(hash_result)
+
+
+app = Flask(__name__)
+app.secret_key = str(gen_hash())
 
 # our index route will handle rendering our form
 @app.route('/')
 def index():
     return render_template("index.html")
 
-#this is for testing purposes
-@app.route('/rh/<int:byte_size>')
-def gen_randomHash(byte_size):
-    print("Remove this after testing. Don't expose your secret key!")
-    print(os.urandom(byte_size))
-    return redirect('/')
 
-@app.route('/result', methods=['POST'])
+@app.route('/users', methods=['POST'])
 def create_user():
     print("Got Post Info")
     print(request.form)
-    first_name_from_form= request.form['first_name']
-    last_name_from_form= request.form['last_name']
-    location_from_form= request.form['location']
-    fav_lang_from_form= request.form['fav_lang']
-    comments_from_form= request.form['comments']
-    ref_from_form= request.form.get('ref', False)
-    
+    session['first_name'] = request.form['first_name']
+    session['last_name'] = request.form['last_name']
+    session['location'] = request.form['location']
+    session['fav_lang'] = request.form['fav_lang']
+    session['comments'] = request.form['comments']
+    session['ref'] = request.form.get('ref', False)
+
     comm_opt= []
     for val in request.form:
         if request.form[val] == "on":
             comm_opt.append(val)
     
-    return render_template("show.html", first_name_on_template= first_name_from_form, last_name_on_template= last_name_from_form, location_on_template= location_from_form, fav_lang_on_template= fav_lang_from_form, comments_on_template= comments_from_form, ref_on_template= ref_from_form, comm_opt= comm_opt)
+    session['comm_options'] = comm_opt
+
+    return redirect("/show")
+
+
+@app.route('/show')
+def show_user():
+    return render_template("show.html")
 
 
 if __name__ == "__main__":
